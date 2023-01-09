8a2055c87559f9a865638924fb01781313995101
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-01-09 13:40:19 +0530

    fourth

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div style={{border:"2px solid grey", fontSize:"25px" ,paddingLeft:"100px", display:"flex", backgroundColor:"#edf2f7"}}>
-        <Link href="/"><div>Soumalya Ghoshal</div></Link>
+        <div>Soumalya Ghoshal</div>
         <Link href="/projects"><div style={{marginLeft:"100px",textDecoration:"none"}}>Projects</div></Link>
     </div>
   )
